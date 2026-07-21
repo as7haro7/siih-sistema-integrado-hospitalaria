92d71b01a7b869567e34644d68b4b8ed905db6e0
BaluChop <saulchoque123@gmail.com>
2026-07-21 10:07:01 -0400

    Update next.config.ts

--- frontend_siih/next.config.ts
+++ frontend_siih/next.config.ts
@@ -7,7 +7,33 @@ const nextConfig: NextConfig = {
     // Permite compilar a producción ignorando errores de tipado
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        // Aplica estos encabezados a todas las rutas de la aplicación
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://siih-backend.onrender.com",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
-
